da5f24e0afc5a3982cbe4e6c82bbee4cb05e5071
huiyifyj <jxfengyijie@gmail.com>
2018-11-28 15:44:42 +0800

    Upload shell file installing git and move its .gitconfig file to $HOME

---
+++ git/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+# .gitconfig file path
+gitconfigFile="$HOME/.gitconfig"
+
+# Color
+red='\e[1;31m'
+green='\e[0;32m'
+yellow='\e[0;33m'
+plain='\e[0m'
+
+installGit () {
+    # Install git
+    if [ `whoami` == 'root' ]
+    then
+        sudo apt install git
+    else
+        apt install git
+    fi
+}
+
+moveGitconfig () {
+    # Move .gitconfig file to $HOME
+    if [ -f $gitconfigFile ]
+    then
+        echo -e "${red} 文件存在 ${plain}"
+    else
+        echo "文件不存在"
+    fi
+}
+
+if test ! $(which git)
+then
+    installGit
+fi
+
+moveGitconfig
