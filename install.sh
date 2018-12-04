f0cb5c58f4f9cd6fbc92f33a5eff053f7edcc9ec
huiyifyj <jxfengyijie@gmail.com>
2018-12-05 00:19:01 +0800

    Update the method of identifying linux name

--- install.sh
+++ install.sh
@@ -9,30 +9,29 @@ yellow='\e[0;33m'
 plain='\e[0m'
 
 echo "Start to initialize your Linux"
-echo -e "-----------------------------------------------------------\n"
+echo -e "\n-----------------------------------------------------------\n"
 
 getDistribution () {
-    linux=""
 
     # Every system that we officially support has /etc/os-release
 	if [ -r /etc/os-release ]
     then
-		linux=$(. /etc/os-release && echo "$ID")
+		linuxName=$(. /etc/os-release && echo "$ID")
 	fi
 
 	# Returning an empty string here should be alright since the
 	# case statements don't act unless you provide an actual value
-	echo "Your linux distribution is ${linux}"
+	echo -e "Your linux distribution is ${linuxName}\n"
 }
 
 getDistribution
 
 initializeConfig () {
-    echo "-----------------------------------------------------------"
+    echo -e "-----------------------------------------------------------\n"
     echo -e "${green} Install git and its configuration file."
 }
 
-if [ ${linux} == "ubuntu" ] || [ ${linux} == "raspbian" ] || [ ${linux} == "debian" ] || [ ${linux} == "deepin" ]
+if [ ${linuxName}=="ubuntu|raspbian|debian|deepin" ]
 then
     initializeConfig
 else
