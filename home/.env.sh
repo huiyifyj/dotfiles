6ada69551b2e5762d365b2d8707fe844a31cdfdf
huiyifyj <jxfengyijie@gmail.com>
2023-08-13 03:16:50 +0800

    Update `$APP` variable and `rust` installation directory
    - Update $APP variable: /opt => $HOME/APP.
    - Update the specified rust and cargo installation directory.
    - Update comments for `.env.sh` file.

--- home/.env.sh
+++ home/.env.sh
@@ -7,16 +7,14 @@
 # See https://www.gnupg.org/(it)/documentation/manuals/gnupg/Common-Problems.html
 # export GPG_TTY=$(tty)
 
-# Application in /opt folder
-# Authorize to non-root user by
-#   $ sudo chown fyj:fyj -R /opt
-APP=/opt
+# Application in $HOME/app folder
+APP=$HOME/APP
 
 # $APP_BIN folder is used to store soft links pointing to executable files
 APP_BIN=$APP/bin
 export PATH="$APP_BIN:$PATH"
 
-# Go language ENV PATH
+# Go language env PATH
 GOPATH=$HOME/.go # go get download path
 GOROOT=$APP/go
 GOPROXY="https://goproxy.io,direct"
@@ -24,39 +22,38 @@ export GOPATH GOROOT GOPROXY
 export PATH="$GOPATH/bin:$GOROOT/bin:$PATH"
 # Go setting for arm (for Raspbian on raspberry pi)
 # GOOS=linux
-# GOARCH=arm
-# GOARM=6
-# export GOOS GOARCH GOARM
+# GOARCH=arm64
+# export GOOS GOARCH
 
 # nvm, Node Version Manager
-export NVM_DIR="$APP/nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 # Load nvm bash_completion for bash
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Rust custom installation directory
-# More to https://github.com/rust-lang/rustup.rs/issues/618
-RUSTUP_HOME=$APP/rust/rustup
-CARGO_HOME=$APP/rust/cargo
+# Refer to https://github.com/rust-lang/rustup.rs/issues/618
+RUSTUP_HOME=$HOME/.rust/rustup
+CARGO_HOME=$HOME/.rust/cargo
 export RUSTUP_HOME CARGO_HOME
-# Cargo installation directory
-# More to https://doc.rust-lang.org/cargo/commands/cargo-install.html
-CARGO_INSTALL_ROOT=$APP/rust/cargo-install
+# Cargo custom installation directory
+# Refer to https://doc.rust-lang.org/cargo/commands/cargo-install.html
+CARGO_INSTALL_ROOT=$HOME/.rust/cargo-install
 export CARGO_INSTALL_ROOT
 export PATH="$CARGO_HOME/bin:$CARGO_INSTALL_ROOT/bin:$PATH"
 
-# JDK ENV setting, recommend to use AdoptOpenJDK
+# JDK env setting, recommend to use AdoptOpenJDK
 # Download from https://adoptopenjdk.net/releases.html
 JAVA_HOME=$APP/jdk/jdk8 # JDK8 env variable
 # JAVA_HOME=$APP/jdk/jdk11 # JDK11 env variable
 export JAVA_HOME
 export PATH=${JAVA_HOME}/bin:$PATH
 
-# Gradle ENV setting
+# Gradle env setting
 GRADLE_HOME=$APP/gradle
 export PATH="$GRADLE_HOME/bin:$PATH"
 
-# Lua env
+# Lua env setting
 LUA_HOME=$APP/lua
 export PATH="$LUA_HOME/bin:$PATH"
 # Download from http://luabinaries.sourceforge.net/download.html
@@ -68,10 +65,10 @@ export PATH="$LUA_HOME/bin:$PATH"
 #   $ ln -s libreadline.so.7.0 libreadline.so.6
 # Reference link: https://github.com/electron-userland/electron-builder/issues/993
 
-# Flutter ENV Setting
+# Flutter env Setting
 export FLUTTER=$APP/flutter
 export PATH="$FLUTTER/bin:$PATH"
-# Flutter setting.
+# Flutter mirror setting
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
 # export PUB_HOSTED_URL="https://mirrors.tuna.tsinghua.edu.cn/dart-pub"
