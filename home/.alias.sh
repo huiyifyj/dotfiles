8e146c3ec42017c11f1e353510a3911193b1ed09
huiyifyj <jxfengyijie@gmail.com>
2023-08-14 03:59:26 +0800

    Add command line abbreviations and aliases
    - Add `docker` and `git` abbreviations.
    - Set gs alias to `git status` and avoid using running `ghostscript`.
    - Use `lsd` instead of built-in `ls` and set related aliases.

--- home/.alias.sh
+++ home/.alias.sh
@@ -5,3 +5,19 @@ alias ssp="ss -lntup"
 alias psu="ps hax -o user | sort | uniq -c | sort -hr"
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
+
+# docker abbreviation
+alias d="docker"
+# git abbreviation
+alias g="git"
+
+# avoid switching to `ghostscript` when using `gs`
+alias gs="g s"
+
+# Using [lsd](https://github.com/lsd-rs/lsd) instead of `ls`
+if command -v lsd >/dev/null 2>&1; then
+    alias ls="lsd"
+    alias ll="lsd -lh"
+    alias la="lsd -al"
+    alias lt="lsd -A --tree --depth 2 --ignore-glob node_modules --ignore-glob .git"
+fi
