2c4761eb535cc158fb5b79b3d1e4de8453ecd357
huiyifyj <jxfengyijie@gmail.com>
2023-12-19 14:43:07 +0800

    Speed up clone with shallow clone
    Append `--depth=1` flag option to speed up clone. [^1]
    The --depth creates a shallow clone with a history truncated to the specified number of commits.
    
    Refer to "GitHub blog post: Get up to speed with partial clone" [^2] and "GitLab docs for partial clone" [^3] and "Git Documentation: partial-clone" [^4].
    
    ---
    
    [^1]: https://git-scm.com/docs/git-clone/2.43.0#Documentation/git-clone.txt---depthltdepthgt
    [^2]: https://github.blog/2020-12-21-get-up-to-speed-with-partial-clone-and-shallow-clone/
    [^3]: https://docs.gitlab.com/ee/topics/git/partial_clone.html
    [^4]: https://git-scm.com/docs/partial-clone/2.43.0

--- zsh/preinstall.sh
+++ zsh/preinstall.sh
@@ -13,16 +13,16 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 #     Powerline Font must be installed and used in your terminal (for example, switch font to Fira Code).
 
 # Install zsh-completions
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions --depth=1
 # Enable it in your .zshrc by adding it to your plugin list and reloading the completion:
 #     plugins=(… zsh-completions)
 #     autoload -U compinit && compinit
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --depth=1
 # Activate the plugin in ~/.zshrc:
 #     plugins=(... zsh-syntax-highlighting)
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions --depth=1
 # Add the plugin to the list of plugins for Oh My Zsh to load (inside ~/.zshrc):
 #     plugins=(... zsh-autosuggestions)
